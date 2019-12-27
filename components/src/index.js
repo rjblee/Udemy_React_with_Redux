a9bf88526ad9ff0fcfd1af8e5cab418b33c9c5dc
Ralph <ralph.lee@live.ca>
2019-12-27 15:54:33 +0000

    Add 2 more comments

--- components/src/index.js
+++ components/src/index.js
@@ -21,6 +21,38 @@ const App = () => {
                     </div>
                 </div>
             </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Ralph
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Dec 27, 2019 2:30PM</span>
+                    </div>
+                    <div className="text">
+                        Here are the comments!
+                    </div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Ralph
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Dec 27, 2019 2:30PM</span>
+                    </div>
+                    <div className="text">
+                        Here are the comments!
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };

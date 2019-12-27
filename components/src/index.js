9fcd489045d8c4fd2435df35e7a7e0854839113a
Ralph <ralph.lee@live.ca>
2019-12-27 05:47:17 +0000

    Edit App component to show a comment container

--- components/src/index.js
+++ components/src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>THIS IS WORKING</div>
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src="https://cdn.motor1.com/images/mgl/GwZbJ/s3/logo-story-volkswagen.jpg"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Ralph
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Dec 27, 2019 2:30PM</span>
+                    </div>
+                    <div className="text">
+                        Here are the comments!
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'))

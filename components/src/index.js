5d20709773966ee901b91ed15d373e9c2c3c5413
Ralph <ralph.lee@live.ca>
2020-01-02 07:18:39 +0000

    Import and add CommentDetail component

--- components/src/index.js
+++ components/src/index.js
@@ -1,58 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Ralph
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Dec 27, 2019 2:30PM</span>
-                    </div>
-                    <div className="text">
-                        Here are the comments!
-                    </div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Ralph
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Dec 27, 2019 2:30PM</span>
-                    </div>
-                    <div className="text">
-                        Here are the comments!
-                    </div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Ralph
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Dec 27, 2019 2:30PM</span>
-                    </div>
-                    <div className="text">
-                        Here are the comments!
-                    </div>
-                </div>
-            </div>
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
         </div>
     );
 };

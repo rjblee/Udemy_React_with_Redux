05f1d0fb2dad1b00d503e495e94f85f46a1e79dd
Ralph <ralph.lee@live.ca>
2020-01-03 08:59:26 +0000

    Add timeAgo props

--- components/src/index.js
+++ components/src/index.js
@@ -5,11 +5,11 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Ralph" />
-            <CommentDetail author="Kelly" />
-            <CommentDetail author="Scott" />
-            <CommentDetail author="Adam" />
-            <CommentDetail author="Lauren" />
+            <CommentDetail author="Ralph" timeAgo="Today 6:00PM" />
+            <CommentDetail author="Kelly" timeAgo="Today 5:00PM" />
+            <CommentDetail author="Scott" timeAgo="Today 2:00PM" />
+            <CommentDetail author="Adam" timeAgo="Today 12:00PM" />
+            <CommentDetail author="Lauren" timeAgo="Today 6:00AM" />
         </div>
     );
 };

7873e33d21314e853b99bb01738be2297483070c
Ralph <ralph.lee@live.ca>
2020-01-03 08:56:22 +0000

    Add author props

--- components/src/index.js
+++ components/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail author="Ralph" />
+            <CommentDetail author="Kelly" />
+            <CommentDetail author="Scott" />
+            <CommentDetail author="Adam" />
+            <CommentDetail author="Lauren" />
         </div>
     );
 };

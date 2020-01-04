810c92f6b4e96d6be734a81d31c05bee552284ac
Ralph <ralph.lee@live.ca>
2020-01-04 03:31:18 +0000

    Add message prop

--- components/src/index.js
+++ components/src/index.js
@@ -5,11 +5,11 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Ralph" timeAgo="Today 6:00PM" />
-            <CommentDetail author="Kelly" timeAgo="Today 5:00PM" />
-            <CommentDetail author="Scott" timeAgo="Today 2:00PM" />
-            <CommentDetail author="Adam" timeAgo="Today 12:00PM" />
-            <CommentDetail author="Lauren" timeAgo="Today 6:00AM" />
+            <CommentDetail author="Ralph" timeAgo="Today 6:00PM" message="Great post!" />
+            <CommentDetail author="Kelly" timeAgo="Today 5:00PM" message="I love it!" />
+            <CommentDetail author="Scott" timeAgo="Today 2:00PM" message="How are you doing?" />
+            <CommentDetail author="Adam" timeAgo="Today 12:00PM" message="Happy New Year" />
+            <CommentDetail author="Lauren" timeAgo="Today 6:00AM" message="Wish you the best" />
         </div>
     );
 };

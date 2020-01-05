00b17a8c39ed127b32e2047e49c101977d7cde00
Ralph <ralph.lee@live.ca>
2020-01-05 09:47:35 +0000

    Import faker library and add avatar prop into CommentDetail

--- components/src/index.js
+++ components/src/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Ralph" timeAgo="Today 6:00PM" message="Great post!" />
-            <CommentDetail author="Kelly" timeAgo="Today 5:00PM" message="I love it!" />
-            <CommentDetail author="Scott" timeAgo="Today 2:00PM" message="How are you doing?" />
-            <CommentDetail author="Adam" timeAgo="Today 12:00PM" message="Happy New Year" />
-            <CommentDetail author="Lauren" timeAgo="Today 6:00AM" message="Wish you the best" />
+            <CommentDetail 
+                author="Ralph" 
+                timeAgo="Today 6:00PM" 
+                message="Great post!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail 
+                author="Kelly" 
+                timeAgo="Today 5:00PM" 
+                message="I love it!" 
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail 
+                author="Scott" 
+                timeAgo="Today 2:00PM" 
+                message="This is the new post" 
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail 
+                author="Adam" 
+                timeAgo="Today 12:00PM" 
+                message="Happy New Year" 
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail 
+                author="Lauren" 
+                timeAgo="Today 6:00AM" 
+                message="Wish you the best" 
+                avatar={faker.image.avatar()}
+            />
         </div>
     );
 };

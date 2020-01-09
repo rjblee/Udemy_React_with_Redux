9f1ba60cabc4462955caa2746b2ad7062a43a72c
Ralph <ralph.lee@live.ca>
2020-01-09 05:55:44 +0000

    Creat a new React app

---
+++ seasons/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>Hi thereeee</div>;
+};
+
+
+ReactDOM.render(
+    <App></App>, document.querySelector('#root')
+);

61a9fb3cd4011a5ccdc03238c115a63edd0cebd9
Ralph <ralph.lee@live.ca>
2020-01-12 05:51:04 +0000

    Change App component  from functional component to class based component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
+const App extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return <div>Hi thereeee</div>;
+        this.state = { lat: null };
+    }
+
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+    }
+    return <div>Latitude: </div>;
 };
 
 

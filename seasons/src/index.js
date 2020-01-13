c398d9d6ec6f2ca427edade4929a9f65a2fd08eb
Ralph <ralph.lee@live.ca>
2020-01-13 14:26:29 +0000

    Refactor render to have conditional rendering

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -18,13 +18,15 @@ class App extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                Latitude: {this.state.lat}
-                <br></br>
-                Error: {this.state.errorMessage}
-            </div>
-        )
+        if (this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        if (this.state.errorMessage) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        
+        return <div>Things are loading...</div>
     }
 };
 

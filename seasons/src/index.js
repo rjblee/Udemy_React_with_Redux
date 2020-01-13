38fb164e3a97317100652cadf7300c4037d30956
Ralph <ralph.lee@live.ca>
2020-01-13 06:40:07 +0000

    Refactor App component to use state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App extends React.Component {
+class App extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = { lat: null };
-    }
 
-    render() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
+            (position) => {
+                this.setState({ lat: position.coords.latitude});
+            },
             (err) => console.log(err)
         );
     }
-    return <div>Latitude: </div>;
-};
 
+    render() {
+    return <div>Latitude: {this.state.lat}</div>;
+    }
+};
 
 ReactDOM.render(
     <App></App>, document.querySelector('#root')

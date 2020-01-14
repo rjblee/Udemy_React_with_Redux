8c42d719159c89339afe45171e0f9989666eb5cb
Ralph <ralph.lee@live.ca>
2020-01-14 09:48:22 +0000

    Import and contain SeasonDisplay.js

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
     state = { lat: null, errorMessage: '' };
@@ -17,7 +18,7 @@ class App extends React.Component {
 
     render() {
         if (this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
         if (this.state.errorMessage) {

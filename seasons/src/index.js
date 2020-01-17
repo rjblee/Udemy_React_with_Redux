702f6ba3931b801ba0556626c50683c4221d9422
Ralph <ralph.lee@live.ca>
2020-01-17 12:04:48 +0000

    Import Spinner component into render

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
 
 class App extends React.Component {
     state = { lat: null, errorMessage: '' };
@@ -25,7 +27,7 @@ class App extends React.Component {
             return <div>Error: {this.state.errorMessage}</div>
         }
 
-        return <div>Things are loading...</div>
+        return <Spinner></Spinner>;
     }
 };
 

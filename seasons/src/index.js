949a9dd9bb5b7c23bed790fbdedbbe7b61062ea1
Ralph <ralph.lee@live.ca>
2020-01-19 16:48:52 +0000

    Refactor render out to renderContent function

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -18,16 +18,20 @@ class App extends React.Component {
         console.log('My component was just updated - it rerendered');
     }
 
-    render() {
+    renderContent() {
         if (this.state.lat) {
             return <SeasonDisplay lat={this.state.lat} />
         }
-
+    
         if (this.state.errorMessage) {
             return <div>Error: {this.state.errorMessage}</div>
         }
-
+    
         return <Spinner message="Please accept the location request"></Spinner>;
+    }
+
+    render() {
+        return <div>{this.renderContent()}</div>
     }
 };
 

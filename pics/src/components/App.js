bf8070c5d59a19b746d5490e55b0de635138ce33
Ralph <ralph.lee@live.ca>
2020-02-03 08:04:06 +0000

    Add async await

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,13 +4,15 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
     
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID 02b341a79d53289e4a2d3d015309bd9b3d3c517b2b651f745bb538e387f85038'
             }
         });
+    
+        console.log(response.data.results);
     }
     
     render() {

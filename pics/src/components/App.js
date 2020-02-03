3ebc021839a56ded103e2bd4dfb6193ae8fafa11
Ralph <ralph.lee@live.ca>
2020-02-03 07:23:49 +0000

    Add Unsplash API

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 02b341a79d53289e4a2d3d015309bd9b3d3c517b2b651f745bb538e387f85038'
+            }
+        });
     }
     
     render() {

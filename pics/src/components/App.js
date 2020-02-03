030d0d3ee9f22461c780afaaa7d34e7dc39afe46
Ralph <ralph.lee@live.ca>
2020-02-03 09:31:03 +0000

    Add state to show the count of images searched

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    
-    async onSearchSubmit(term) {
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
@@ -12,13 +13,14 @@ class App extends React.Component {
             }
         });
     
-        console.log(response.data.results);
+        this.setState({ images: response.data.results });
     }
     
     render() {
         return (
             <div className="ui container" style={{ marginTop: '20px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         )
     }

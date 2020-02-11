645521a659498e3ed1bd8cc0a0d651180c10e66c
Ralph <ralph.lee@live.ca>
2020-02-11 08:21:47 +0000

    Add styling to ImageList

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,3 +1,4 @@
+import './ImageList.css';
 import React from 'react';
 
 
@@ -6,7 +7,7 @@ const ImageList = (props) => {
         return <img alt={image.description} key={image.id} src={image.urls.regular} />;
     });
 
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
 }
 
 export default ImageList;
